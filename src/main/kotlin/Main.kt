2f121c91ac1f00d2a1221d8179c1519784e4aaa1
seraphh01 <71792603+seraphh01@users.noreply.github.com>
2022-12-12 19:06:59 +0200

    Lab 7

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 fun main() {
     val g = Grammar("src/main/resources/g1.in")
     println("Nonterminals: ${g.nonTerminals}")
@@ -17,6 +19,19 @@ fun main() {
 
     println(g.getEnrichedGrammar().productionSet.getOrderedProductions())
     println(lr.getParsingTable())
+    var result = "";
+    try {
+        val parseTree = lr.parse(listOf("a", "b", "b", "c")).sortedBy { it.index }
+
+        for (row in parseTree) {
+            result += "${row.index}: ${row.info}, ${row.parent}, ${row.rightSibling}\n"
+        }
+    } catch (e: Exception) {
+        println(e.message)
+    }
+    print(result)
+
+    File("out.txt").writeText(result);
 }
 
 
